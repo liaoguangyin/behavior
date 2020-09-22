907c679372b58bdb5c41a51e28d1890cd979d461
光银 廖 <2357125311@qq.com>
2020-09-22 20:14:11 +0800

    每日

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -87,9 +87,6 @@
 
 --select author,avg(reward) from problem group by author having avg(reward)<10 order by avg(reward) asc
 
---以Problem中的数据为基础，使用SELECT INTO，新建一个Author和Reward都没有NULL值的新表：NewProblem （把原Problem里Author或Reward为NULL值的数据删掉）
---    使用INSERT SELECT, 将Problem中Reward为NULL的行再次插入到NewProblem中
-
 --    select *from NewProblem
 --    drop table [user]
 --select author,reward
@@ -99,16 +96,96 @@
 --insert NewProblem 
 --select reward from problem 
 --alter table profile add teacherid int
-create table profile
-(
-[id] int primary key identity,
-[name] NVARCHAR(10) ,
-[teacher] NVARCHAR(10) ,
-)
-create table [user]
-(
-[id] int primary key ,
-[name]NVARCHAR(10),
-[teacherid] int foreign key references profile(id),
-)
+
+--select *into newproblem from problem where author is not null and Reward is not null
+
+--insert NewProblem (author,reward) select author,reward from problem where Reward is null
  
+ --create table problem1
+ --(
+ --id int primary key identity(1,1)
+ --)
+
+ --select * from [user]
+ --create table[profile]
+ --(
+ --id int primary key identity,
+ --[gender] bit,
+ --birthyear int,
+ --birthmonth nvarchar(30),
+ --birthstar nvarchar(30),
+ --myself nvarchar(100),
+ --)
+ --drop table [profile]
+
+ --create table [user](
+ --[id] int primary key identity(1,1),
+ --[username] nvarchar(30),
+ --[password] int,
+ --inviteedby int constraint fk_user_invitedby foreign key references [user](id) ,
+ --profileid int constraint fk_user_profileid foreign key references [profile](id),
+ --)
+ --alter table [user]
+ --add constraint uk_user_profileid unique(profileid)
+-- select *from profile 
+-- insert [profile] values(2019,7,N'巨蟹座',1234,N'自我介绍1')
+-- insert [profile] values(2020,7,N'摩羯座',2344,N'自我介绍2')
+-- insert [profile] values(2019,7,N'水瓶座',1784,N'自我介绍3')
+-- insert [profile] values(2019,7,N'双子座',1277,N'自我介绍4')
+
+-- insert [user] values (N'张三',1234,null,1)
+-- insert [user] values (N'李四',1234,null,2)
+-- insert [user] values (N'王五',1234,null,3)
+-- insert [user] values (N'赵六',1234,null,4)
+
+--insert [profile] values (2019,7,N'金牛座',1267,N'自我介绍5')
+
+--delete [profile] where id=5
+--delete [user] where profileid =5
+
+--create table credit(
+--id int primary key identity(1,1),
+--[user] nvarchar(30),
+--[time] datetime,
+--reason nvarchar(50),
+--num int,
+--) 
+--alter table credit
+--alter column [user] int; 
+--alter table credit
+--add constraint fk_user_userid foreign key([user]) references[user](id)
+
+--alter table problem add [author] nvarchar(20);
+
+--alter table problem 
+--add constraint fk_user_uid foreign key([uid]) references [user](id)
+
+--select * from problem
+--alter table problem
+--drop column author
+--insert problem values (N'晋',null,1,23,'2020/4/7',null,4)
+--insert problem values (N'清',null,2,24,'2020/6/7',null,5)
+--insert problem values (N'宋',null,6,24,'2020/9/7',null,6)
+--insert problem values (N'商',null,7,43,'2020/12/7',null,7)
+
+--update problem set uid=5 where id2=6
+--delete problem where [uid]=4;
+--delete [user] where id=6
+
+--create table keyword2 
+--(
+--id int primary key identity(1,1),
+--kword nvarchar(50),
+--)
+--create table keywordtoproblem
+--(
+--keywordid int constraint fk_keywordtoproblem_keywordid foreign key references keyword2(id),
+--problemid int constraint fk_keywordtoproblem_problemid foreign key references problem(id)
+--)
+--select COUNT(keywordid),keywordid from  keywordtoproblem where keywordid=2
+--group by keywordid
+
+
+
+
+
