94017d52afac58e13a30f1453427dd7a451330db
光银 廖 <2357125311@qq.com>
2020-10-11 16:11:29 +0800

    111

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            name = null;
+            Console.WriteLine(Console.ReadLine());
+            int password;
+            password = 1;
+            Console.WriteLine(Console.ReadLine());
+        }
+    }
+}
