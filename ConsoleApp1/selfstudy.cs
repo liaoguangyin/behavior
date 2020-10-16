83087ecebe5919c1bfbea19d38b044df3e9f8343
光银 廖 <2357125311@qq.com>
2020-10-16 20:55:16 +0800

    作业

---
+++ ConsoleApp1/selfstudy.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Dynamic;
+
+namespace ConsoleApp1
+{
+    class Selfstudy
+    {
+        static void Main(string[] args)
+        {
+            //作业
+            //观察一起帮登录页面，用if...else ...完成以下功能。
+            //用户依次由控制台输入：验证码、用户名和密码：
+            //如果验证码输入错误，直接输出：“*验证码错误”；
+            //如果用户名不存在，直接输出：“*用户名不存在”；
+            //如果用户名或密码错误，输出：“*用户名或密码错误”
+            //以上全部正确无误，输出：“恭喜！登录成功！”
+            //const string code = "zzz", name = "廖", password = "4567";
+            //Console.WriteLine("输入验证码");
+            //string usecode = Console.ReadLine();
+            //if (usecode == code)
+            //{
+            //    Console.WriteLine("输入用户名");
+            //    string usename = Console.ReadLine();
+            //    if (usename == name)
+            //    {
+            //        Console.WriteLine("输入密码");
+            //        string usepassword = Console.ReadLine();
+            //        if (usepassword == password)
+            //        {
+            //            Console.WriteLine("登陆成功");
+            //        }
+            //        else
+            //        {
+            //            Console.WriteLine("用户名或密码错误");
+            //        }
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("用户名不存在");
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("验证码错误");
+            //}
+            //作业：
+            //      将源栈同学姓名 / 昵称分别：
+            //      按进栈时间装入一维数组，
+            //      按座位装入二维数组，
+            //    并输出共有多少名同学。
+            //string[] students ={ "刘伟-刘", "龚廷义-龚", "李智博-李", "周丁浩-周", "廖光银-廖" };
+            //Console.WriteLine(students.Length);
+            //string[,] student = new string[2,4];
+            //student[0,0]  = "刘伟-刘";
+            //student[0, 1] = "龚廷义-龚";
+            //student[0, 2] = "李智博-李";
+            //student[0, 3] = "周丁浩-周";
+            //student[0, 4] = "廖光银-廖";
+            //student[1, 0] = "第一位";
+            //student[1, 1] = "第二位";
+            //student[1, 2] = "第三位";
+            //student[1, 3] = "第四位";
+            //student[1, 4] = "第五位";
+            //Console.WriteLine();
+            //    作业：
+            //         分别用for循环和while循环输出：1,2,3,4,5 和 1,3,5,7,9
+            //         用for循环输出存储在一维 / 二维数组里的源栈所有同学姓名 / 昵称
+            //         让电脑计算并输出：99 + 97 + 95 + 93 + ...+1的值
+            //         将源栈同学的成绩存入一个double数组中，用循环找到最高分和最低分
+            //         找到100以内的所有质数（只能被1和它自己整除的数）
+            //         生成一个元素（值随机）从小到大排列的数组
+            //int i = 0;
+            //while (i < 6)
+            //{
+            //    i++;
+            //    Console.WriteLine(i);
+            //}
+            int i = 0;
+            while (i < 10)
+            {
+                i++;
+                i = (i*2 - 1);
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
