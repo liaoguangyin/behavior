69cec42a396a3f8dee65c5e3ce54a8028f3bf110
光银 廖 <2357125311@qq.com>
2020-10-28 15:52:53 +0800

    作业User 属性修改

--- ConsoleApp1/user.cs
+++ ConsoleApp1/user.cs
@@ -9,17 +9,42 @@ namespace selfstudy
     //注册/登录功能，定义一个User类，包含字段：Name（用户名）、Password（密码）和 邀请人（InvitedBy），和方法：Register()、Login()
     class user
     {
-        private string Name;
-        private string Password;
-        private user Invitedby;
-
-        public static bool Register(user user) 
-        {
-        
+        private string _Name;
+        private user()
+        { 
         }
-        public static bool Login(user user)
+        public user(string name, string password)
         {
-
+            _Name = name;
+            _password = password;
         }
+        public string name 
+        {
+            set 
+            {
+                if (value=="admin")
+                {
+                    _Name = "系统管理员";
+                }
+                else
+                {
+                    _Name = value;
+                }
+            }
+            get 
+            {
+                return _Name;
+            }
+        }
+        private string _password;
+        public string password 
+        {
+            set;
+            private get;
+        }
+
+        public user _invitedBy { get; set; }
+        public string _inviteCode { get; set; }
+        public string AuthCode { get; set; }
     }
 }
